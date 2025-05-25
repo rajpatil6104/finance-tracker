c010d93ecb8417096c9875e4384996f5a9b23130
rajpatil6104 <132218873+rajpatil6104@users.noreply.github.com>
2025-05-25 23:34:39 +0530

    Fix build issues and verified local build

--- src/App.js
+++ src/App.js
@@ -1,23 +1,12 @@
-import logo from './logo.svg';
-import './App.css';
+import TransactionForm from "./TransactionForm";
+import TransactionList from "./TransactionList";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div style={{ padding: "20px" }}>
+      <h1>Monthly Finance Tracker</h1>
+      <TransactionForm />
+      <TransactionList />
     </div>
   );
 }
